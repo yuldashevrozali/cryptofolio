fbe4aa74d2b6e301b9050aee3b75005c921369b5
Yuldashevrozali <Yuldashevrozali08@gmail.com>
2024-04-17 19:17:47 +0500

    heaer add

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import dropdown from '../../assets/dropdown.svg';
+import './index.css';
+
+export default function Header() {
+  const [selectedOption, setSelectedOption] = useState('USD');
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+  };
+
+  console.log(selectedOption);
+
+  return (
+    <>
+    <div className="header-top">
+    <div className="header-logo">
+        <h1>CRYPTOFOLIO</h1>
+      </div>
+      <div className="header-watchlist">
+        <div className="dropdown">
+          <button className="dropbtn">{selectedOption}<span><img src={dropdown} alt="dropdown" /></span></button>
+          <div className="dropdown-content">
+            <a onClick={() => handleSelect('USD')}>USD</a>
+            <a onClick={() => handleSelect('USD2')}>USD2</a>
+            <a onClick={() => handleSelect('USD3')}>USD3</a>
+          </div>
+        </div>
+        <button className = 'watchlistbtn'>watchlist</button>
+      </div>
+    </div>
+    <div className="header-bottom">
+      
+    </div>
+    </>
+  );
+}
